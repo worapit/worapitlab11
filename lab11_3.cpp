39ee50d8d325d6e340559fa2d57bc1b194296f71
worapit <drm.wmwork@gmail.com>
2021-12-17 18:05:09 +0700

    complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ifstream score;
+    score.open("score.txt");
+    string A,B;
+    int i = 0 ;
+    float sum = 0.0,sum2 = 0.0;
+    while(getline(score, A)){
+        sum = sum + stof(A.c_str());
+        sum2 = sum2 + stof(A.c_str())*stof(A.c_str()) ;
+        i++;
+    }
+
+    float mean, m2, sd;
+    mean = sum/i ;
+    m2 = sum2/i;
+    sd = sqrt(m2 - pow(mean,2));
+
+
+    cout << "Number of data = " << i << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << sd;
+
+ 
+}
